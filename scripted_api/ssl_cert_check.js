14e7a169b3ff40094db9c53ea93ae48207c7779e
pnvnd <peter@datacrunch.ca>
2023-01-17 10:28:12 -0500

    added: SSL cert check

---
+++ scripted_api/ssl_cert_check.js
@@ -0,0 +1,9 @@
+let $secure = process.env; // comment out when testing in New Relic
+var assert = require("assert")
+var sslChecker = require("ssl-checker")
+
+// EDIT: Put your custom URL here "example.com"
+const url = 'example.com'
+// EDIT: Days before your certification expires to fail the check and provide an alert
+const failDaysBeforeExpiration = 30
+sslChecker(url, 'GET', 443).then(result => assert.ok(result.daysRemaining > failDaysBeforeExpiration, "the certificate will expire in the next " + result.daysRemaining + " days."));
