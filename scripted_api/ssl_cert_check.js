420f7f089266397880e1097a1ed01b17cd4fe3df
pnvnd <peter@datacrunch.ca>
2023-02-16 17:11:00 -0500

    updated SSL check script

--- scripted_api/ssl_cert_check.js
+++ scripted_api/ssl_cert_check.js
@@ -1,5 +1,6 @@
 let $secure = process.env; // comment out when testing in New Relic
-// var assert = require("assert")
+
+var assert = require("assert")
 var sslChecker = require("ssl-checker")
 
 // EDIT: Put your custom URL here "example.com"
@@ -8,6 +9,21 @@ const url = 'datacrunch.ca';
 // EDIT: Days before your certification expires to fail the check and provide an alert
 const failDaysBeforeExpiration = 30;
 
-sslChecker(url, 'GET', 443)
-  .then(result => console.info(result));
-//  .then(result => assert.ok(result.daysRemaining > failDaysBeforeExpiration, "the certificate will expire in the next " + result.daysRemaining + " days."));
+async function checkSsl() {
+  let resp = await sslChecker(url, 'GET', 443);
+    console.log(resp);
+  
+    // Push the incident details to New Relic, comment out when testing locally.
+    $util.insights.set('daysRemaining', resp.daysRemaining);
+    $util.insights.set('valid', resp.valid);
+    $util.insights.set('validFor', resp.validFor[0]);
+    $util.insights.set('validFrom', resp.validFrom);
+    $util.insights.set('validTo', resp.validTo);
+
+    // SELECT latest(custom.daysRemaining), latest(custom.valid), latest(custom.validFor), latest(custom.validFrom), latest(custom.validTo) 
+    // FROM SyntheticCheck WHERE monitorName = "Monitor Name Here"
+
+    assert.ok(resp.daysRemaining > failDaysBeforeExpiration, "The certificate will expire in the next " + resp.daysRemaining + " days.");
+  };
+
+checkSsl();
