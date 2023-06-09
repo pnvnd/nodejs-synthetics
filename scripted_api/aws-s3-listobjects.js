e4ff572d66ca8b425b61d1f34073ed309aeda28c
pnvnd <peter@datacrunch.ca>
2023-06-09 16:47:22 -0400

    added: custom s3 detail check

--- scripted_api/aws-s3-listobjects.js
+++ scripted_api/aws-s3-listobjects.js
@@ -1,23 +1,71 @@
 let $secure = process.env; // comment out when testing in New Relic
-
-// Load the AWS SDK for Node.js
+var got = require('got');
 var AWS = require('aws-sdk');
-// Set the region 
-AWS.config.update({region: 'ca-central-1'});
 
 // Create S3 service object
-s3 = new AWS.S3({apiVersion: '2006-03-01'});
-
-// Create the parameters for calling listObjects
-var bucketParams = {
-  Bucket : 'www.datacrunch.ca',
-};
-
-// Call S3 to obtain a list of the objects in the bucket
-s3.listObjects(bucketParams, function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", data);
+var s3 = new AWS.S3(
+  {
+    apiVersion: '2006-03-01',
+    region: 'ca-central-1',
+    accessKeyId: $secure.AWS_ACCESS_KEY_ID,
+    secretAccessKey: $secure.AWS_SECRET_ACCESS_KEY
   }
-});
+);
+
+// Function to list bucket names and retrieve bucket details
+function listBucketsAndBucketDetails() {
+  s3.listBuckets(function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      var bucketNames = data.Buckets.map(bucket => bucket.Name);
+
+      // Get bucket details for each bucket
+      var bucketDetails = bucketNames.map(bucketName => getBucketDetails(bucketName));
+
+      Promise.all(bucketDetails)
+        .then(results => {
+          console.log(results);
+
+          var account_id = $secure.ACCOUNT_ID;
+
+          got.post(`https://insights-collector.newrelic.com/v1/accounts/${account_id}/events`, {
+            headers: {
+              'Api-Key': $secure.NEW_RELIC_LICENSE_KEY,
+              'Content-Type': 'application/json'
+            },
+            json: results
+          });
+        })
+        .catch(err => {
+          console.log("Error retrieving bucket details:", err);
+        });
+    }
+  });
+}
+
+// Function to get bucket details
+function getBucketDetails(bucketName) {
+  return new Promise((resolve, reject) => {
+    s3.listObjectsV2({ Bucket: bucketName }, function(err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        var totalSize = data.Contents.reduce((acc, obj) => acc + obj.Size, 0);
+        var totalObjects = data.KeyCount;
+
+        var bucketDetails = {
+          "eventType": "s3CustomSample",
+          "bucketName": bucketName,
+          "bucketSizeBytes": totalSize,
+          "numberOfObjects": totalObjects
+        };
+
+        resolve(bucketDetails);
+      }
+    });
+  });
+}
+
+// Call the function to list bucket names and retrieve bucket details
+listBucketsAndBucketDetails();
